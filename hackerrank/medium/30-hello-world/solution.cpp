f25960bbe959c0eabcd55536ccc02107fd112606
mustafa64-stack <mustafa.93717@gmail.com>
2026-08-11 14:40:06 +0530

    ✅ Day 0: Hello, World. | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ hackerrank/medium/30-hello-world/solution.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    string c;
+    getline(cin,c);
+    cout<<"Hello, World.\n"<<c;
+    return 0;
+}
