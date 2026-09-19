d62ab02f1f067f829a127d46e58bb84d062780da
Mustafa <mustafa.93717@gmail.com>
2026-09-20 01:26:50 +0530

    ✅ Palindrome Number | Easy | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-09-19

---
+++ leetcode/easy/0009-palindrome-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long rev_num=0;
+    int num=x;
+    int last_digit;
+    while(num>0){
+        last_digit=num%10;
+        rev_num=rev_num*10+last_digit;
+        num/=10;
+    }
+    if(rev_num==x){
+        return true;
+    }return false;
+    }
+};
